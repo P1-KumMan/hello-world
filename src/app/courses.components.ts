6027f12a8ba233a6e0d5e01b8d44bbd035c191e8
P1-KumMan <adithan92014@gmail.com>
2020-11-11 08:57:31 +0530

    new start

--- src/app/courses.components.ts
+++ src/app/courses.components.ts
@@ -6,14 +6,16 @@ import { CoursesService } from './courses.service';
     template:`
     <h2>{{title}}</h2>
     <ul><li *ngFor="let course of courses">{{course}}</li></ul>
+    <button class="btn btn-primary"[class.active]="isActive">Save<button>
     `
 })
 
 export class CoursesComponent{
+    isActive=true;
     title="List of courses"
     courses;
 
-    constructor(service: CoursesService){
+    constructor(service: CoursesService){   
         this.courses = service.getCourses();
     }
 }
